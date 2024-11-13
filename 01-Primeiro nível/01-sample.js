3b39c04806c695e60fdaf9f4428f6584f0766721
juliareboucas <juliareboucasjbarros@gmail.com>
2024-11-13 14:57:23 -0300

    refatorar 01-sample do 01-Primeiro nivel

--- 01-Primeiro nível/01-sample.js
+++ 01-Primeiro nível/01-sample.js
@@ -1,13 +1,21 @@
 function achaPares(listaNumeros) {
+
     const listaPares = [];
+    const DIVISIVEL = 2;
     for (let i = 0; i < listaNumeros.length; i++) {
-        if (listaNumeros[i] % 2 === 0) {
+        if (listaNumeros[i] % DIVISIVEL === 0) {
             listaPares.push(listaNumeros[i]);
         }
     }
     return listaPares;
 }
+const AVALIADO1 = 1;
+const AVALIADO2 = 2;
+const AVALIADO3 = 3;
+const AVALIADO4 = 4;
+const AVALIADO5 = 5;
+const AVALIADO6 = 6;
 
-const numerosAvaliados = [1, 2, 3, 4, 5, 6];
+const numerosAvaliados = [AVALIADO1, AVALIADO2, AVALIADO3, AVALIADO4, AVALIADO5, AVALIADO6];
 const numerosPares = achaPares(numerosAvaliados);
 console.log(numerosPares);
