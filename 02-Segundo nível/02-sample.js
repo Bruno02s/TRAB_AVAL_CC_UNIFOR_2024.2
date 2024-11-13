5cf71d8b80d5f3b6686aaac982680edebc97d7b3
profmiqueias <miqueiasmaia@unifor.br>
2024-11-13 19:11:23 -0300

    fix: sample2

--- 02-Segundo nível/02-sample.js
+++ 02-Segundo nível/02-sample.js
@@ -1,18 +1,19 @@
 const IDADE_MINIMA_PARA_DIRIGIR = 18;
+const IDADE = 16;
 
 function validarIdade(idade) {
-    if (typeof idade !== 'number' || idade <= 0 || idade !== idade) { 
-        throw new Error('Idade inválida. A idade deve ser um número positivo.');
+    if (typeof idade !== "number" || idade <= 0 || idade !== idade) { 
+        throw new Error("Idade inválida. A idade deve ser um número positivo.");
     }
 }
 
 function verificarSePodeDirigir(idade) {
     validarIdade(idade);  
-    return idade >= IDADE_MINIMA_PARA_DIRIGIR ? 'Pode dirigir' : 'Não pode dirigir';
+    return idade >= IDADE_MINIMA_PARA_DIRIGIR ? "Pode dirigir" : "Não pode dirigir";
 }
 
 try {
-    console.log(verificarSePodeDirigir(16));  
+    console.log(verificarSePodeDirigir(IDADE));  
 } catch (erro) {
     console.error(erro.message); 
 }
