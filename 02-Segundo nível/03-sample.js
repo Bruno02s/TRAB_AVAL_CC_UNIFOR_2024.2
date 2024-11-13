c9aef6f0de5504a89e2f250e0b1c2cb54b39eeaa
profmiqueias <miqueiasmaia@unifor.br>
2024-11-13 19:13:30 -0300

    fix: sample3

--- 02-Segundo nível/03-sample.js
+++ 02-Segundo nível/03-sample.js
@@ -1,8 +1,9 @@
 const VALOR_POR_KM = 10;
+const VALOR = 50;
 
 function validarDistancia(distancia) {
-    if (typeof distancia !== 'number' || distancia <= 0 || distancia !== distancia) { 
-        throw new Error('Distância inválida. A distância deve ser um número positivo.');
+    if (typeof distancia !== "number" || distancia <= 0 || distancia !== distancia) { 
+        throw new Error("Distância inválida. A distância deve ser um número positivo.");
     }
 }
 
@@ -12,7 +13,7 @@ function calcularFrete(distancia) {
 }
 
 try {
-    const valorFrete = calcularFrete(50);
+    const valorFrete = calcularFrete(VALOR);
     console.log(`O valor do frete é: R$ ${valorFrete}`);
 } catch (erro) {
     console.error(erro.message);  
